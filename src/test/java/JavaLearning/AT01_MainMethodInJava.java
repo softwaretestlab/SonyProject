89ee12703abe0403dd46e6490e81fc90aec3e6da
softwaretestlab <softwaretestlab@hotmail.com>
2022-10-20 20:10:44 +1100

    new

---
+++ src/test/java/JavaLearning/AT01_MainMethodInJava.java
@@ -0,0 +1,12 @@
+//package
+package JavaLearning;
+    //class
+public class AT01_MainMethodInJava {
+    // method ---> main method
+        public static void main(String[] args){
+            //code
+            //sout-enterkey
+            System.out.println("This is my First Java Class");
+            System.out.println("This is my Second Java Class");
+        }
+}
---
+++ src/test/java/JavaLearning/AT02_JunitMethods.java
@@ -0,0 +1,17 @@
+package JavaLearning;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AT02_JunitMethods {
+
+    @Test
+    public void test01(){
+        System.out.println("This is method 1");
+    }
+    @Test
+    public void test02(){
+        System.out.println("This is method 2");
+    }
+
+}
