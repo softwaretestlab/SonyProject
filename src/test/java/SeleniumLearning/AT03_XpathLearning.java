cbaadb7290c42c3364b5b61bf6a7a7c7f8dfabb9
softwaretestlab <softwaretestlab@hotmail.com>
2022-11-07 20:24:36 +1100

    xpath

--- src/test/java/SeleniumLearning/AT03_XpathLearning.java
+++ src/test/java/SeleniumLearning/AT03_XpathLearning.java
@@ -2,9 +2,12 @@ package SeleniumLearning;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 public class AT03_XpathLearning {
@@ -22,12 +25,20 @@ public class AT03_XpathLearning {
         driver.get("https://www.myer.com.au/join");
         //implicit wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //enter email address
         //xpath version-1
         driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Test@gmail.com");
+        //click on create account button
         ////xpath version-2
         //button[contains(@class,'MuiButtonBase-root MuiButton-root MuiButton-c') and contains(@type,'button')]
         driver.findElement(By.xpath("//button[contains(@class,'MuiButtonBase-root MuiButton-root MuiButton-c')]")).click();
+       //Enter password
+        driver.findElement(By.xpath("//input[@id='password' and @type='password']")).sendKeys("password");
+        //Click on create account button
+        WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Create account')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        Thread.sleep(20000);
         //xpath version-3
-        driver.findElement(By.xpath("//span[contains(text(),'Create Account')]")).click();
+        driver.findElement(By.xpath("//button[@id='create-account']")).click();
     }
 }
