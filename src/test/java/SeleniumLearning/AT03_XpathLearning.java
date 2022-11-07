75823cc13999518c5744bf7484c174604686ccbb
softwaretestlab <softwaretestlab@hotmail.com>
2022-11-07 19:55:15 +1100

    xpath

---
+++ src/test/java/SeleniumLearning/AT03_XpathLearning.java
@@ -0,0 +1,33 @@
+package SeleniumLearning;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+public class AT03_XpathLearning {
+
+    @Test
+    public void xpath() throws InterruptedException {
+        //Setting up chromedriver.
+        WebDriverManager.chromedriver().setup();
+        //we create an object of WebDriver
+        WebDriver driver = new ChromeDriver();
+        //At this point browser will be opened
+        //maximize browser
+        driver.manage().window().maximize();
+        // navigate to HN website
+        driver.get("https://www.myer.com.au/join");
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //xpath version-1
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Test@gmail.com");
+        ////xpath version-2
+        //button[contains(@class,'MuiButtonBase-root MuiButton-root MuiButton-c') and contains(@type,'button')]
+        driver.findElement(By.xpath("//button[contains(@class,'MuiButtonBase-root MuiButton-root MuiButton-c')]")).click();
+        //xpath version-3
+        driver.findElement(By.xpath("//span[contains(text(),'Create Account')]")).click();
+    }
+}
